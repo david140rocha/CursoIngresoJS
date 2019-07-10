101bb1309d7a2efe53429d00d82e0960ca0f94ed
david140rocha <52495180+david140rocha@users.noreply.github.com>
2019-07-10 20:07:56 -0300

    IF 7--

---
+++ jsInstruccionIF (7).js
@@ -0,0 +1,26 @@
+function mostrar()
+{
+//tomo la edad  
+var edad;
+var estado;
+
+edad = parseInt(document.getElementById("edad").value);
+
+estado = document.getElementById("estadoCivil").value;
+
+if (edad < 18)
+{
+    alert("Es muy pequeño para NO ser soltero");
+}
+if (estado |= "casado","Divorsiado" )
+{
+
+
+    alert("Es muy pequeño para NO ser soltero");
+    
+}
+
+	
+
+
+}//FIN DE LA FUNCIÓN
