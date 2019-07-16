7cad0a8c5deb6227ceddccb3b46a500faf5034ac
david140rocha <52495180+david140rocha@users.noreply.github.com>
2019-07-16 18:12:54 -0300

    wh1

---
+++ jsIteraciones (2).js
@@ -0,0 +1,9 @@
+function mostrar()
+{ var numero = 1;
+   var contador = 10 ;
+   while  (contador >10);{
+   numero = numero - 1
+   contador = contador - 1
+	alert(contador);
+}
+}//FIN DE LA FUNCIÓN
