8a94edb1c27778ba6924c3fe26adb59048141d42
david140rocha <52495180+david140rocha@users.noreply.github.com>
2019-08-01 00:01:30 -0300

    terminado

--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -2,23 +2,53 @@ function mostrar()
 {
 //tomo la edad  
 var hora = document.getElementById('hora').value;
+    switch(hora){
+        case '7':
+        case '8':
+        case '9':
+        case '10':
+        case '11':
 
     if (hora >= 7 && hora <=11 )
     {
       alert ('Es de mañana.');
     }
-    else if ( hora >= 12 && hora <= 19)
+    break;
+    case '12':
+    case '13':
+    case '14':
+    case '15':
+    case '16':
+    case '17':
+    case '18':
+    case '19': 
+    if ( hora >= 12 && hora <= 19)
      {
-        alert ('Es de tarde.');
+        alert ('Es tarde.');
     }
-    else if ( hora >= 20 && hora <= 24 || hora >= 0 && hora <= 6) 
+    break;
+    case '20':
+    case '21':
+    case '22':
+    case '23':
+    case '24':
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    if ( hora >= 20 && hora <= 24 || hora >= 0 && hora <= 6) 
     {
         alert('Es de noche');
     }
-    if (!(hora >= 0 && hora <= 24)) 
-    { 
-        alert ('la hora no existe.');
-    }
+    break
 
+    default:    //if (!(hora >= 0 && hora <= 24)) {}
+        alert ('la hora no existe.');
+    break
+     
+    }
 
 }//FIN DE LA FUNCIÓN
